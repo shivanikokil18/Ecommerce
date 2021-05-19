ba7762bee6bdf56cdaacb05ac5209cd3b1ee6ee1
shivanikokil18 <shivani.kokil@mindbodyonline.com>
2021-05-19 23:32:38 +0530

    controller demo

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,11 @@
 class ApplicationController < ActionController::Base
+   # before_action :require_login
+#private
+
+ # def require_login
+  #  unless logged_in?
+   #   flash[:error] = "You must be logged in to access this section"
+    #  redirect_to new_user_registration_url # halts request cycle
+   # end
+  #end
 end
